a4a464173bf6350335b9d0262a2e4e71dd39b37f
AnesVijay <2011andreyka@gmail.com>
2023-11-07 17:49:34 +0500

    fixed VolumeChanging

--- Assets/_Scripts/Music and sounds/AudioOnGameStart.cs
+++ Assets/_Scripts/Music and sounds/AudioOnGameStart.cs
@@ -5,23 +5,21 @@ namespace BreadFlip.Sound
 {
     public class AudioOnGameStart : MonoBehaviour
     {
+        [SerializeField] AudioMixer mixer;
 
-        [SerializeField] AudioMixerGroup backgroundMixer;
-        [SerializeField] AudioMixerGroup effectsMixer;
-
-        void Awake()
+        void Start()
         {
             if (PlayerPrefs.HasKey("CurrentBackVolume"))
                 if (PlayerPrefs.GetInt("CurrentBackVolume") == 1)
-                    backgroundMixer.audioMixer.SetFloat("BackVolume", 0f);
+                    mixer.SetFloat("BackVolume", 0f);
                 else
-                    backgroundMixer.audioMixer.SetFloat("BackVolume", -80f);
+                    mixer.SetFloat("BackVolume", -80f);
 
             if (PlayerPrefs.HasKey("CurrentEffectsVolume"))
                 if (PlayerPrefs.GetInt("CurrentEffectsVolume") == 1)
-                    backgroundMixer.audioMixer.SetFloat("EffectsVolume", 0f);
+                    mixer.SetFloat("EffectsVolume", 0f);
                 else
-                    backgroundMixer.audioMixer.SetFloat("EffectsVolume", -80f);
+                    mixer.SetFloat("EffectsVolume", -80f);
         }
     }
 }
