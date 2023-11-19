2ab37c52bb89e47c308b98b37687fd22c86a1eed
Filippov Stepan <43007025+debebantur@users.noreply.github.com>
2023-11-19 17:03:40 +0500

    DB working interface (#6)
    * added id saving
    
    * test scene fixes

--- Assets/_Scripts/DataBase/DBInterface.cs
+++ Assets/_Scripts/DataBase/DBInterface.cs
@@ -6,19 +6,36 @@ namespace BreadFlip
 {
     public static class DBInterface
     {
-        public static bool RegisterPlayer(string name)
+        public static int RegisterPlayer(string name)
         {
             var a = new DBPlayer();
             a.player = name;
-            return DBWorker.PutRecord(a);
+            var id = DBWorker.PutRecord(a);
+            Debug.Log(id);
+            PlayerPrefs.SetInt("PlayerId", id);
+            PlayerPrefs.SetString("PlayerName", name);
+            if(id != -1)
+                PlayerPrefs.SetInt("PlayerRecord", 0);
+            return id;
         }
 
-        public static bool UpdateRecord(string name, int record)
+        public static bool UpdateRecord(int record)
         {
-            var a = new DBPlayer();
-            a.player = name;
+            Debug.Log("updating record");
+            var prevRecord = PlayerPrefs.GetInt("PlayerRecord", 0);
+            Debug.Log("current record" + prevRecord.ToString());
+            if (prevRecord > record) return false;
+            Debug.Log("continue changing");
+            var a = new DBRegPlayer();
+            PlayerPrefs.SetInt("PlayerRecord", record);
+            a.player = PlayerPrefs.GetInt("PlayerId", 404);
             a.record = record;
-            return DBWorker.PutRecord(a);
+            var ok = DBWorker.UpdateRecord(a);
+            if (!ok)
+                PlayerPrefs.SetInt("RetryRecord", 1);
+            else
+                PlayerPrefs.SetInt("RetryRecord", 0);
+            return true;
         }
 
         public static DBTopRecords GetRecords()
