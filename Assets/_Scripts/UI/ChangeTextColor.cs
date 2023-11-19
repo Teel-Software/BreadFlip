80c09b5b08c28de4668ba8c43ca4f403ebf38d8f
AnesVijay <2011andreyka@gmail.com>
2023-11-19 16:15:31 +0500

    ui-updates --> main
    commit ccf3ad9f0d15f682bb1a80e156d329c36e6f197f
    Author: AnesVijay <2011andreyka@gmail.com>
    Date:   Sun Nov 19 16:07:08 2023 +0500
    
        added script for changing text color on buttons
    
    commit 456472f39434c4d4bbc8461b2ba27b9f50aafdad
    Author: AnesVijay <2011andreyka@gmail.com>
    Date:   Sun Nov 19 16:02:02 2023 +0500
    
        change settings panel ui

---
+++ Assets/_Scripts/UI/ChangeTextColor.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BreadFlip.UI
+{
+    public class ChangeTextColor : MonoBehaviour
+    {
+        public Color defaultColor;
+        public Color pressedColor;
+
+        [SerializeField] private TMP_Text buttonText;
+
+        public void ChangeColorOnPressed()
+        {
+            buttonText.color = pressedColor;
+        }
+
+        public void ChangeColorToDefault()
+        {
+            buttonText.color = defaultColor;
+        }
+    }
+}
