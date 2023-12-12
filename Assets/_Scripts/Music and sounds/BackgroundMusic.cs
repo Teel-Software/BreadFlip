68eb8fa9910c14c01bafdf63cdef650c16333404
Filippov Stepan <43007025+debebantur@users.noreply.github.com>
2023-12-12 19:51:50 +0500

    changed script (#18)

--- Assets/_Scripts/Music and sounds/BackgroundMusic.cs
+++ Assets/_Scripts/Music and sounds/BackgroundMusic.cs
@@ -7,37 +7,54 @@ namespace BreadFlip.Sound
 {
     public class BackgroundMusic : MonoBehaviour
     {
+        public static BackgroundMusic instance;
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(transform.root.gameObject);
+                GetComponent<AudioSource>().Play();
+            }
+            else
+            {
+                if (instance != this)
+                {
+                    Destroy(transform.root.gameObject, 0.2f);
+                }
+            }
+        }
 
-#region Вариант 1
-        // private static BackgroundMusic backgroundMusic;
-        // private void Awake()
-        // {
-        //     DontDestroyOnLoad(gameObject);
-        //     if (backgroundMusic == null)
-        //     {
-        //         backgroundMusic = this;
-        //     }
-        //     else
-        //     {
-        //         Destroy(gameObject);
-        //     }
-        // }
-#endregion
+            #region Вариант 1
+            // private static BackgroundMusic backgroundMusic;
+            // private void Awake()
+            // {
+            //     DontDestroyOnLoad(gameObject);
+            //     if (backgroundMusic == null)
+            //     {
+            //         backgroundMusic = this;
+            //     }
+            //     else
+            //     {
+            //         Destroy(gameObject);
+            //     }
+            // }
+            #endregion
 
-#region Вариант 2
-        // private AudioSource _audioSource;
+            #region Вариант 2
+            // private AudioSource _audioSource;
 
-        // private void Awake() {
-        //     DontDestroyOnLoad(gameObject);
-        //     _audioSource = GetComponent<AudioSource>();
-        //     PlayMusic();
-        // }
-        
-        // private void PlayMusic()
-        // {
-        //     if (_audioSource.isPlaying) return;
-        //     _audioSource.Play();
-        // }
-#endregion
-    }
+            // private void Awake() {
+            //     DontDestroyOnLoad(gameObject);
+            //     _audioSource = GetComponent<AudioSource>();
+            //     PlayMusic();
+            // }
+
+            // private void PlayMusic()
+            // {
+            //     if (_audioSource.isPlaying) return;
+            //     _audioSource.Play();
+            // }
+            #endregion
+        }
 }
