6bd01079f3f7ed924b543a98bcb6a75406eb61cb
Alexey Kucherenko <lex.keivi@mail.ru>
2024-01-14 21:21:53 +0500

    Comments in skin changer and some other mehtods

--- Assets/_Scripts/Entities/Skins/ToastSkinChanger.cs
+++ Assets/_Scripts/Entities/Skins/ToastSkinChanger.cs
@@ -14,22 +14,43 @@ namespace BreadFlip.Entities.Skins
 
         private void Awake()
         {
+            // скин, который ставится по дефолту, при старте игры
             CurrentSkin = _defaultSkin;
             ChangeSkin(CurrentSkin);
         }
 
         private void Update()
         {
+            // переключение скинов. по сути только для демонстриации
             if (Input.GetKeyDown(KeyCode.R))
             {
-                var index = Array.IndexOf(_skins, CurrentSkin);
-                var nextIndex = (index + 1) % _skins.Length;
-
-                var nextSkin = _skins[nextIndex];
-                ChangeSkin(nextSkin);
+                NextSkin();
             }
         }
 
+        public void NextSkin()
+        {
+            var index = Array.IndexOf(_skins, CurrentSkin);
+            var nextIndex = (index + 1) % _skins.Length;
+
+            var nextSkin = _skins[nextIndex];
+            ChangeSkin(nextSkin);
+        }
+
+        public void PreviousSkin()
+        {
+            var index = Array.IndexOf(_skins, CurrentSkin);
+            var previousIndex = index - 1;
+            if (index < 0)
+            {
+                previousIndex = _skins.Length - 1;
+            }
+
+            var previousSkin = _skins[previousIndex];
+            ChangeSkin(previousSkin);
+        }
+
+        // основная функция. нужно передать тост из списка _skins, чтобы включить
         public void ChangeSkin(Toast newSkin)
         {
             CurrentSkin = newSkin;
@@ -41,6 +62,24 @@ namespace BreadFlip.Entities.Skins
             foreach (var skinNeedy in _skinNeedies)
             {
                 skinNeedy.UpdateSkin(newSkin);
+            }
+        }
+        
+        public void ChangeSkin(int newSkinIndex)
+        {
+            if (newSkinIndex <= 0 || newSkinIndex >= _skins.Length)
+                return;
+
+            CurrentSkin = _skins[newSkinIndex];
+            
+            foreach (var skin in _skins)
+            {
+                skin.gameObject.SetActive(skin == CurrentSkin);
+            }
+
+            foreach (var skinNeedy in _skinNeedies)
+            {
+                skinNeedy.UpdateSkin(CurrentSkin);
             }
         }
     }
