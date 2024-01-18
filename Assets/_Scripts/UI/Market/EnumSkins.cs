622124befd22f18ee24a560f8f89a536a7e334c0
AnesVijay <2011andreyka@gmail.com>
2024-01-18 15:09:21 +0500

    added enum for skins names

---
+++ Assets/_Scripts/UI/Market/EnumSkins.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace BreadFlip.UI
+{
+    public enum Skins
+    {
+        DefaultSkin = 0,
+        NotDefaultSkin = 12,
+        CatSkin = 15,
+        CorgiAssSkin = 100
+    }
+}
